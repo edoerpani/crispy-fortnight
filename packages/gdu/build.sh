a95243daa8915147fe7ff5fa1a6f614ed8c6055c
edoerpani <erpaniedo6@gmail.com>
2024-08-23 00:49:54 +0800

    Initialize 10638-package-procmail

---
+++ packages/gdu/build.sh
@@ -0,0 +1,22 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/dundee/gdu
+TERMUX_PKG_DESCRIPTION="Modern and intuitive terminal-based text editor"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="5.14.0"
+TERMUX_PKG_SRCURL=https://github.com/dundee/gdu/archive/v$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=6fb64500eb22a4e586322c065e97bed72b6f4e1aead4311dad50b75d11222cdb
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_make() {
+	return
+}
+
+termux_step_make_install() {
+	termux_setup_golang
+
+	make build
+	install -D dist/gdu -t $TERMUX_PREFIX/bin
+	make gdu.1
+	install -Dm644 gdu.1 -t $TERMUX_PREFIX/share/man/man1
+}
---
+++ packages/geth/build.sh
@@ -0,0 +1,31 @@
+TERMUX_PKG_HOMEPAGE=https://geth.ethereum.org/
+TERMUX_PKG_DESCRIPTION="Go implementation of the Ethereum protocol"
+TERMUX_PKG_LICENSE="LGPL-3.0, GPL-3.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="1.10.18"
+TERMUX_PKG_SRCURL=https://github.com/ethereum/go-ethereum/archive/v$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=de358812c89e64b6b64f9dc6bd95a830902e3161959038e9d76be506f57ecef5
+TERMUX_PKG_AUTO_UPDATE=true
+
+termux_step_make() {
+	termux_setup_golang
+
+	export GOPATH=$TERMUX_PKG_BUILDDIR
+	mkdir -p "${GOPATH}"/src/github.com/ethereum
+	ln -sf "$TERMUX_PKG_SRCDIR" "$GOPATH"/src/github.com/ethereum/go-ethereum
+
+	cd "$GOPATH"/src/github.com/ethereum/go-ethereum
+	for applet in geth abigen bootnode ethkey evm rlpdump puppeth; do
+		(cd ./cmd/"$applet" && go build -v)
+	done
+	unset applet
+}
+
+termux_step_make_install() {
+	for applet in geth abigen bootnode ethkey evm rlpdump puppeth; do
+		install -Dm700 \
+			"$TERMUX_PKG_SRCDIR/cmd/$applet/$applet" \
+			"$TERMUX_PREFIX"/bin/
+	done
+	unset applet
+}
