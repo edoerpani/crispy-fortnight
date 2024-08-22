d77d221d1c12109a32b012ce068fd11170d65c88
edoerpani <erpaniedo6@gmail.com>
2024-08-23 00:54:04 +0800

    Initialize libuv-self-exe

---
+++ packages/teleport-tsh/build.sh
@@ -0,0 +1,31 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/gravitational/teleport
+TERMUX_PKG_DESCRIPTION="Secure Access for Developers that doesn't get in the way"
+TERMUX_PKG_LICENSE="Apache-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="16.0.3"
+TERMUX_PKG_SRCURL=https://github.com/gravitational/teleport/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=5973b503df85cb493d9625abaee74587eaf724e04d1d9c6ba687828a623eb0ac
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_HOSTBUILD=true
+
+termux_step_host_build() {
+	termux_setup_golang
+	pushd "$TERMUX_PKG_SRCDIR"
+
+	# from Makefile
+	export KUBECTL_VERSION=$(go run ./build.assets/kubectl-version/main.go)
+	popd
+}
+
+termux_step_make() {
+	termux_setup_golang
+	export GOPATH=$TERMUX_PKG_CACHEDIR/go
+	export BUILDDIR=$TERMUX_PKG_SRCDIR/cmd
+
+	make $BUILDDIR/tsh
+}
+
+termux_step_make_install() {
+	install -Dm700 -t $TERMUX_PREFIX/bin $BUILDDIR/tsh
+}
