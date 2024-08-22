7c1ced122a1445b4bd37ccc30fea8ac1e9659ae5
edoerpani <erpaniedo6@gmail.com>
2024-08-23 00:54:32 +0800

    Initialize nodejs-v22.5.1

---
+++ packages/vtm/build.sh
@@ -0,0 +1,16 @@
+TERMUX_PKG_HOMEPAGE=https://vtm.netxs.online/
+TERMUX_PKG_DESCRIPTION="Terminal multiplexer with TUI window manager and multi-party session sharing"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_LICENSE_FILE="LICENSE"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="0.9.98"
+TERMUX_PKG_SRCURL=https://github.com/netxs-group/vtm/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=f51c2d2c210d3a73461f5f5e157e0e21f6f9fb774da784d68a9cd1f498e251c3
+TERMUX_PKG_DEPENDS="libc++"
+TERMUX_PKG_BUILD_DEPENDS="libandroid-spawn"
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+
+termux_step_pre_configure() {
+	CXXFLAGS+=" -pthread"
+}
---
+++ x11-packages/praat/build.sh
@@ -0,0 +1,18 @@
+TERMUX_PKG_HOMEPAGE=https://www.praat.org
+TERMUX_PKG_DESCRIPTION="Doing phonetics by computer"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="6.4.14"
+TERMUX_PKG_SRCURL=https://github.com/praat/praat/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=d44fc310401f838da12d03b0b358f271bf00d3eac385f5dd6cb302dc0c6a364b
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="atk, gdk-pixbuf, glib, gtk3, harfbuzz, libc++, libcairo, pango, pulseaudio, zlib"
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_post_get_source() {
+	rm -f meson.build
+}
+
+termux_step_make_install() {
+	install -Dm700 -t $TERMUX_PREFIX/bin praat
+}
